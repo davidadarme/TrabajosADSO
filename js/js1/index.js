5af41db512149a07be7206c48364943fb0c577b5
unknown <timdavidarm@pm.me>
2023-10-19 21:07:33 -0500

    js

---
+++ js/js1/index.js
@@ -0,0 +1,8 @@
+document.write('<RH>');
+document.write("Nigga.com");
+document.write('<RH>');
+
+var pt = new java.awt.Point(3, 5);
+pt.transalation(7, 5);
+document.writeIn("<p>");
+document.writeIn("Translating (3,5) by (7,5) yields (" + pt.x + "," + pt.y + ").");
